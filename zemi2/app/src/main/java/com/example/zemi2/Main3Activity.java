bd42a4a6ca47d87643e64e57cf9c00d9336fa49e
daichaso <45479076+daichaso@users.noreply.github.com>
2019-11-13 05:56:07 +0900

    Add files via upload

---
+++ zemi2/app/src/main/java/com/example/zemi2/Main3Activity.java
@@ -0,0 +1,14 @@
+package com.example.zemi2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
